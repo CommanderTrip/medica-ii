2029bb98d32110f091d91bf5a49b4bc1363241e3
Jason Carnahan <jasoncarnahan@canvas-inc.com>
2023-11-01 08:55:24 -0500

    First Pages

--- for-ras/src/router/index.js
+++ for-ras/src/router/index.js
@@ -1,21 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import LettersView from "../views/LettersView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/Letters",
+      name: "letters",
+      component: LettersView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/DaisyLove",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      // This page loads a lot of images; lets lazy load this
+      component: () => import("../views/DaisyLoveView.vue")
     }
   ]
 })

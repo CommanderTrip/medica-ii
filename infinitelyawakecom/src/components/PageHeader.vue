17f2f73ba7a1310394a3c8cc03d1fa182837ff5e
Trip <cmdrtrip@gmail.com>
2024-04-27 20:52:07 -0500

    Header transtitions

--- infinitelyawakecom/src/components/PageHeader.vue
+++ infinitelyawakecom/src/components/PageHeader.vue
@@ -1,13 +1,60 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="page-header"></div>
+  <div class="page-header">
+    <RouterLink to="/" class="title">Infinitely Awake</RouterLink>
+    <div class="header-routes">
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/static">The Static</RouterLink>
+      <RouterLink to="/events">Events</RouterLink>
+      <RouterLink to="/join">Join Now</RouterLink>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+$header-height: 8rem;
+
+.title {
+  font-size: x-large;
+  text-decoration: none;
+}
+
+.header-routes {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.header-routes a {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-inline: 2rem;
+  height: 100%;
+  width: 25%;
+  text-decoration: none;
+  background-image: linear-gradient(#00000000 0, #ffffffff 100%);
+  background-position-y: $header-height;
+  background-origin: border-box;
+  background-repeat: no-repeat;
+  transition: background-position 0.5s;
+}
+
+a:hover {
+  background-position-y: 100%;
+}
+
 .page-header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
   position: sticky;
-  height: 10rem;
+  height: $header-height;
   background-color: red;
+  padding-inline: 5%;
 }
 </style>

e618a14f3ec7d8666ce9f836e6bfef0e54d04374
Trip <cmdrtrip@gmail.com>
2024-04-28 18:45:37 -0500

    Added title image

---
+++ infinitelyawakecom/src/components/ImageCarousel.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import titleImage from "@/assets/awake-screenshots/wave.jpg";
+</script>
+
+<template>
+  <div class="container">
+    <img :src="titleImage" alt="" />
+    <div class="site-title">
+      <p>Infinitely</p>
+      <p>Awake</p>
+      <em>A Final Fantasy XIV Free Company</em>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+}
+
+.site-title {
+  position: absolute;
+  bottom: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  font-size: xxx-large;
+  padding-inline: 2rem;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  opacity: 33%;
+}
+
+.site-title em {
+  font-size: x-large;
+  font-weight: lighter;
+  text-shadow:
+    0 0 10px #ffffff,
+    0 2px 20px #ffffff;
+}
+</style>
--- infinitelyawakecom/src/views/HomeView.vue
+++ infinitelyawakecom/src/views/HomeView.vue
@@ -3,19 +3,18 @@ import HomeAccordion from "@/components/HomeAccordion.vue";
 import ChapterHeading from "@/components/ChapterHeading.vue";
 import DisplayCard from "@/components/DisplayCard.vue";
 
+import titleImage from "@/assets/awake-screenshots/wave.jpg";
+
 import image1 from "@/assets/awake-screenshots/2I4recB.png";
 import image2 from "@/assets/awake-screenshots/187b6a0672385-screenshotUrl.jpg";
 import image3 from "@/assets/awake-screenshots/2023-05-05_00-29-30_Talim_-_d-d-doppio.png";
 import PageFooter from "@/components/PageFooter.vue";
+import ImageCarousel from "@/components/ImageCarousel.vue";
 </script>
 
 <template>
   <div class="wrapper">
-    <div class="site-title">
-      <p>Infinitely</p>
-      <p>Awake</p>
-      <em>A Final Fantasy XIV Free Company</em>
-    </div>
+    <ImageCarousel />
     <HomeAccordion />
   </div>
   <ChapterHeading title="Welcome" description="You should join us" />
@@ -48,20 +47,6 @@ import PageFooter from "@/components/PageFooter.vue";
   display: flex;
   justify-content: space-between;
   width: 100%;
-  background-color: gray;
   height: var(--page-banner-height);
-}
-
-.site-title {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  font-size: xxx-large;
-  padding-inline: 2rem;
-}
-
-.site-title em {
-  font-size: x-large;
-  font-weight: lighter;
 }
 </style>
